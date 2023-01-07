987503d4f9d27ae5f61bee64ce1cc8c087ba19ea
Jorik <83394320+Raadsl@users.noreply.github.com>
2023-01-07 11:57:22 +0000

    added particles.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var typing=new Typed(".text", { //typing thingy at about me
-       strings: ["", "Fullstack Developer","Python Developer", "Node Developer", "Web Developer", "Discord bot Developer", "Web Designer", "Human", "Programmer", "Minecraft plugin"],
+       strings: ["", "Fullstack Developer","Python Developer", "Node Developer", "Web Developer", "Discord bot Developer", "Web Designer", "Human", "Programmer", "Minecraft plugin developer", "Robot (yes, I'm a robot:)"],
        typeSpeed: 100,
        backSpeed: 40,
        loop: true,
    });
 
 document.getElementById("year").innerHTML = new Date().getFullYear(); //auto updates copyright year
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
