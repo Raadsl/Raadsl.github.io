68d8bb90ef464107e6ac713dfdb135b4463bb847
Jorik <83394320+Raadsl@users.noreply.github.com>
2023-02-19 13:35:48 +0000

    updated projects portfolio; Looks better and is mobile responsive

--- script.js
+++ script.js
@@ -18,12 +18,13 @@ function bouncy() {
   setTimeout(function() { 
      document.getElementById("pfp").classList.remove('bounceNOW');
 }, 2500);
-  setTimeout(function() { 
+  /*setTimeout(function() { 
      document.getElementById("Raadsel").classList.add('rise');
 }, 2500);
+*/
 }
 
-
+//A developer animation
 const slides = document.querySelectorAll('.slide')
 const clearActives = () => {
   slides.forEach(slide => slide.classList.remove('active'))
@@ -35,3 +36,80 @@ const setActive = (e) => {
 slides.forEach(slide => {
   slide.addEventListener('click', setActive)
 })
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+let interval1 = null;
+
+document.querySelector("h2").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 20);
+}
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval1);
+  
+  interval1 = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval1);
+    }
+    
+    iteration += 1 / 3;
+  }, 20);
+}
+
+// onfocus title change
+window.onblur = function() {
+    document.title = 'Raadsel | 👋';
+}
+window.onfocus = function() {
+    document.title = 'Raadsel | Hey';
+}
+bouncy()
+
+//blob
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
+
