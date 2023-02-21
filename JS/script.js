de04da1f4f5fb5e7c69726b832adee5949fcb6dc
Jorik <83394320+Raadsl@users.noreply.github.com>
2023-02-21 13:28:28 +0000

    Some style update, ordered files and minified code

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ var typing=new Typed(".text", { //typing thingy at about me
    });
 document.getElementById("year").innerHTML = new Date().getFullYear(); //auto updates copyright year
 
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
@@ -15,19 +15,19 @@ function bouncy() {
     document.getElementById("pfp").classList.add('bounceNOW');
   }, 500);
   setTimeout(function() { 
-    document.getElementById("pfp").src = "images/pfp.jpeg"
-  }, 1500);
+    document.getElementById("pfp").src = "assets/pfp.jpeg"
+  }, 1900);
   setTimeout(function() { 
     document.getElementById("pfp").classList.remove('bounceNOW');
      //document.getElementById("blob").style.display = "block";
-  }, 3000);
+  }, 2200);
 
 }
 
 function toggleimg() {
   var img = document.getElementById('pfp').src;
-  if (img.indexOf('images/pfp.jpeg')!=-1) {
-    document.getElementById('pfp').src  = 'images/raadsel-empty.svg';
+  if (img.indexOf('assets/pfp.jpeg')!=-1) {
+    document.getElementById('pfp').src  = 'assets/raadsel-empty.svg';
     document.body.style.backgroundColor  = `#2c2626`
     document.body.style.color = `#d3c3c3;`
     document.body.style.color = `#fff;`
@@ -46,7 +46,7 @@ function toggleimg() {
     }
   } 
   else {
-    document.getElementById('pfp').src = 'images/pfp.jpeg';
+    document.getElementById('pfp').src = 'assets/pfp.jpeg';
     document.body.style.backgroundColor  = `#fff`
     document.body.style.color = `#000;`
     const elements = document.getElementsByClassName("large");
@@ -67,35 +67,39 @@ function toggleimg() {
 }
 //A developer animation
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-document.querySelectorAll("h1, h2").forEach(element => {
-  let interval = null;
-  element.onmouseover = event => {  
-    let iteration = 0;
-    clearInterval(interval);
-    interval = setInterval(() => {
-      event.target.innerText = event.target.innerText
-        .split("")
-        .map((letter, index) => {
-          if(index < iteration) { return event.target.dataset.value[index]};
-          return letters[Math.floor(Math.random() * 26)]
-        })
-        .join("");
-      if(iteration >= event.target.dataset.value.length){ 
-        clearInterval(interval);
-      }
-      iteration += 1 / 3;
-    }, 20);
-  }
-});
+let interval = null;
+document.querySelector("h2").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 20);
+}
 
 // onfocus title change
 window.onblur = function() {
-  document.querySelector(`link[rel="icon"]`).href = "/images/raadsel-empty.svg";
+  document.querySelector(`link[rel="icon"]`).href = "/assets/raadsel-empty.svg";
   document.title = 'Thanks for visiting! - Raadsel';
 }
 window.onfocus = function() {
-  document.querySelector(`link[rel="icon"]`).href = "/images/raadsel.svg";
+  document.querySelector(`link[rel="icon"]`).href = "/assets/raadsel.svg";
   document.title = 'Raadsel - Jorik Loos';
 }
 
